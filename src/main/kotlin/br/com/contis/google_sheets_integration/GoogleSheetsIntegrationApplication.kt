72bc081251dd662940beb1aa569fccaff97785d1
Felipe Oliveira <lipefan0@gmail.com>
2025-10-23 13:46:22 -0300

    initial commit

---
+++ src/main/kotlin/br/com/contis/google_sheets_integration/GoogleSheetsIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package br.com.contis.google_sheets_integration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleSheetsIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoogleSheetsIntegrationApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/contis/google_sheets_integration/GoogleSheetsIntegrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.contis.google_sheets_integration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleSheetsIntegrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
